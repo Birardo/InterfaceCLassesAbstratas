76c70a0888fcf9597294556cc95386d4b8ec3a1c
Birardo <j.verderi@edu.senai.br>
2026-09-17 08:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ Foto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceCLassesAbstratas
+{
+    internal class Foto : ISalvavel
+    {
+        public void Salvar()
+        {
+            Console.WriteLine("Salvando a foto em formato .jpg");
+        }
+    }
+}
---
+++ ISalvavel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceCLassesAbstratas
+{
+    internal interface ISalvavel
+    {
+        public void Salvar();
+    }
+}
