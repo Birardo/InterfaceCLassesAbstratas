95e553831c9a3caac1a245c5dc24873fbe1dbd1b
Birardo <j.verderi@edu.senai.br>
2026-09-17 08:38:17 -0300

    Atividade 3

---
+++ Drone.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceCLassesAbstratas
+{
+    internal class Drone : ICamera, IVoavel
+    {
+        public void TirarFoto()
+        {
+            Console.WriteLine("Tirando fotos panorâmicas");
+        }
+        public void Decolar()
+        {
+            Console.WriteLine("Decolando...");
+        }
+        public void Pousar()
+        {
+            Console.WriteLine("Pousando...");
+        }
+    }
+}
---
+++ IVoavel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceCLassesAbstratas
+{
+    internal interface IVoavel
+    {
+        public void Decolar();
+        public void Pousar();
+    }
+}
